5e546e382ac77c8fff41a6ec821f8e11aca75b90
Vlad Bokii <bokiyvladik10@gmail.com>
2025-09-23 16:29:44 +0300

    add form send

--- src/services/booking.js
+++ src/services/booking.js
@@ -2,7 +2,6 @@ import nodemailer from "nodemailer";
 import { getEnvVar } from "../utils/getEnvVar.js";
 
 export const sendBooking = async ({ name, email }) => {
-
   const transporter = nodemailer.createTransport({
     host: getEnvVar("SMTP_HOST"),
     port: Number(getEnvVar("SMTP_PORT")),
@@ -11,19 +10,14 @@ export const sendBooking = async ({ name, email }) => {
       user: getEnvVar("SMTP_USER"),
       pass: getEnvVar("SMTP_PASS"),
     },
-    tls: {
-      rejectUnauthorized: false,
-    },
   });
 
-
   await transporter.sendMail({
-    from: `"MoonPortal" <${getEnvVar("SMTP_USER")}>`,
+    from: `"MoonPortal" <${getEnvVar("FROM_EMAIL")}>`,
     to: getEnvVar("ADMIN_EMAIL"),
     subject: "New booking",
     text: `Name: ${name}\nEmail: ${email}`,
   });
-
 
   return { name, email };
 };
--- src/services/contact.js
+++ src/services/contact.js
@@ -1,22 +1,27 @@
 import nodemailer from "nodemailer";
 import { getEnvVar } from "../utils/getEnvVar.js";
+
 export const sendContactRequest = async ({ name, email, message }) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
+    host: getEnvVar("SMTP_HOST"),
+    port: Number(getEnvVar("SMTP_PORT")),
+    secure: false,
     auth: {
       user: getEnvVar("SMTP_USER"),
       pass: getEnvVar("SMTP_PASS"),
     },
   });
 
-  await transporter.sendMail({
-    from: `"MoonPortal" <${getEnvVar("SMTP_USER")}>`,
+  const mailOptions = {
+    from: `"MoonPortal" <${getEnvVar("FROM_EMAIL")}>`,
     to: getEnvVar("ADMIN_EMAIL"),
     subject: "New Contact Request",
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
     replyTo: email,
-  });
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+  console.log("Message sent:", info);
 
   return { name, email, message };
 };
