749bf0c2f79faf7208f16f03479165eddf6848f4
Vlad Bokii <bokiyvladik10@gmail.com>
2025-09-26 17:31:51 +0300

    update mailnet

--- src/services/booking.js
+++ src/services/booking.js
@@ -1,23 +1,30 @@
-import nodemailer from "nodemailer";
+import Mailjet from "node-mailjet";
 import { getEnvVar } from "../utils/getEnvVar.js";
 
 export const sendBooking = async ({ name, email }) => {
-  const transporter = nodemailer.createTransport({
-    host: getEnvVar("SMTP_HOST"),
-    port: Number(getEnvVar("SMTP_PORT")),
-    secure: false,
-    auth: {
-      user: getEnvVar("SMTP_USER"),
-      pass: getEnvVar("SMTP_PASS"),
-    },
+  const mailjet = Mailjet.apiConnect(
+    getEnvVar("MAILJET_API_KEY"),
+    getEnvVar("MAILJET_API_SECRET")
+  );
+
+  const request = mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: getEnvVar("FROM_EMAIL"),
+          Name: "MoonPortal",
+        },
+        To: [
+          {
+            Email: getEnvVar("ADMIN_EMAIL"),
+          },
+        ],
+        Subject: "New booking",
+        TextPart: `Name: ${name}\nEmail: ${email}`,
+      },
+    ],
   });
 
-  await transporter.sendMail({
-    from: `"MoonPortal" <${getEnvVar("FROM_EMAIL")}>`,
-    to: getEnvVar("ADMIN_EMAIL"),
-    subject: "New booking",
-    text: `Name: ${name}\nEmail: ${email}`,
-  });
-
+  await request;
   return { name, email };
 };
--- src/services/contact.js
+++ src/services/contact.js
@@ -1,27 +1,33 @@
-import nodemailer from "nodemailer";
+import Mailjet from "node-mailjet";
 import { getEnvVar } from "../utils/getEnvVar.js";
 
 export const sendContactRequest = async ({ name, email, message }) => {
-  const transporter = nodemailer.createTransport({
-    host: getEnvVar("SMTP_HOST"),
-    port: Number(getEnvVar("SMTP_PORT")),
-    secure: false,
-    auth: {
-      user: getEnvVar("SMTP_USER"),
-      pass: getEnvVar("SMTP_PASS"),
-    },
+  const mailjet = Mailjet.apiConnect(
+    getEnvVar("MAILJET_API_KEY"),
+    getEnvVar("MAILJET_API_SECRET")
+  );
+
+  const request = mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: getEnvVar("FROM_EMAIL"),
+          Name: "MoonPortal",
+        },
+        To: [
+          {
+            Email: getEnvVar("ADMIN_EMAIL"),
+          },
+        ],
+        Subject: "New Contact Request",
+        TextPart: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+        ReplyTo: {
+          Email: email,
+        },
+      },
+    ],
   });
 
-  const mailOptions = {
-    from: `"MoonPortal" <${getEnvVar("FROM_EMAIL")}>`,
-    to: getEnvVar("ADMIN_EMAIL"),
-    subject: "New Contact Request",
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    replyTo: email,
-  };
-
-  const info = await transporter.sendMail(mailOptions);
-  console.log("Message sent:", info);
-
+  await request;
   return { name, email, message };
 };
